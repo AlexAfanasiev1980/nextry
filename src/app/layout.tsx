a5f3ab3507f992e8b13bc9e770f414835b0908d8
Alexander Afanasiev <armawir1@mail.ru>
2023-11-22 21:38:32 +0700

    add icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "@/styles/globals.scss";
 import "@/styles/mixins.scss";
+import Logo from "./Logo.png";
 import { NextAuthProvider } from "@/components/providers/NextAuthProvider";
 
 export const metadata: Metadata = {
